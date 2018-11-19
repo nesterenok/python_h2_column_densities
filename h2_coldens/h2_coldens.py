e6f6fd5d81f7a3b04363181260b9ba09ef6849f6
Aleksandr <alex-n10@yandex.ru>
2018-11-19 13:04:33 +0300

    1

--- h2_coldens/h2_coldens.py
+++ h2_coldens/h2_coldens.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-label_font = {'family': 'serif',
+label_font = {'family': 'Times New Roman',
         'color':  'black',
         'weight': 'normal',
-        'size': 12,
+        'size': 14,
         }
 
 def plot_h2_coldens(path):
@@ -54,6 +54,7 @@ def plot_h2_coldens(path):
     plt.legend()
     plt.show()
 
+
 def plot_h2_dissociation(path):
     path += "sim_data_h2_chemistry.txt"
     data_z, data_diss = np.loadtxt(path, usecols=(0, 7), comments='!', unpack=True)
@@ -74,12 +75,98 @@ def plot_h2_dissociation(path):
     plt.show()
 
 
+def plot_h2_coldens_compare(path_list, label_list):
+    marker_list = ['o','^', 'v', 's']
+    color_list = ['black', 'red', 'blue', 'green']
+    fig = plt.figure(figsize=(7.,7.))
+    ax = fig.add_subplot(111)
+    
+    for i in range(len(path_list)):
+        path = path_list[i] + "coldens_H2.txt"
+        data_v, data_j, data_en, data_cd = np.loadtxt(path, usecols=(1, 2, 4, 5), comments='!', unpack=True)
+        
+        en = []
+        cd = []
+        for v in range(0,4):    
+            for k in range(len(data_v)):
+                if (v == data_v[k] and v != 0) or (v == 0 and data_v[k] == 0 and data_j[k] != 0 and data_j[k] != 1):
+                     en += [data_en[k]]
+                     cd += [data_cd[k]]
+        ax.scatter(en, cd, s=45, facecolors='none', marker=marker_list[i], color=color_list[i], label=label_list[i])
+         
+    ax.set_yscale('log')
+    ax.set_ylim(1.e+8, 1.e+21)
+    ax.set_xlim(0., 20000.)
 
-#
-#
+    ax.set_xlabel('Level energy, cm$^{-1}$', fontdict=label_font)
+    ax.set_ylabel('Column densities, N/g', fontdict=label_font)
+    ax.tick_params(labelsize=14)
+
+    plt.title('H$_2$ level column densities', fontdict=label_font)
+    plt.legend()
+    plt.show()
+
+
 #
 # path = "../../../output_data_2e4/shock_30_h2-h_lique-bossion/"
 # plot_h2_coldens(path)
 
-path = "../../../output_data_2e4/shock_30_h2-h_lique-bossion/"
-plot_h2_dissociation(path)
+# path = "../../../output_data_2e4/shock_30_h2-h_lique-bossion/"
+# plot_h2_dissociation(path)
+
+path_list = []
+path_list += ['../../../output_data_2e4/shock_20_h2-h_lique-bossion/']
+path_list += ['../../../output_data_2e4/shock_30_h2-h_lique-bossion/']
+path_list += ['../../../output_data_2e4/shock_45_h2-h_lique-bossion/']
+path_list += ['../../../output_data_2e4/shock_55_h2-h_lique-bossion/']
+
+label_list = []
+label_list += ["20 km/s"]
+label_list += ["30 km/s"]
+label_list += ["45 km/s"]
+label_list += ["55 km/s"]
+#plot_h2_coldens_compare(path_list, label_list)
+
+path_list = []
+path_list += ["../../../output_data_2e4/shock_20_h2-h_lique-bossion_cr100/"]
+path_list += ["../../../output_data_2e4/shock_30_h2-h_lique-bossion_cr100/"]
+path_list += ["../../../output_data_2e4/shock_45_h2-h_lique-bossion_cr100/"]
+path_list += ["../../../output_data_2e4/shock_55_h2-h_lique-bossion_cr100/"]
+
+label_list = []
+label_list += ["20 km/s"]
+label_list += ["30 km/s"]
+label_list += ["45 km/s"]
+label_list += ["55 km/s"]
+#plot_h2_coldens_compare(path_list, label_list)
+
+path_list = []
+path_list += ["../../../output_data_2e5/shock_20_h2-h_lique-bossion/"]
+path_list += ["../../../output_data_2e5/shock_30_h2-h_lique-bossion/"]
+path_list += ["../../../output_data_2e5/shock_40_h2-h_lique-bossion/"]
+
+label_list = []
+label_list += ["20 km/s"]
+label_list += ["30 km/s"]
+label_list += ["40 km/s"]
+#plot_h2_coldens_compare(path_list, label_list)
+
+path_list = []
+path_list += ["../../../output_data_2e5/shock_20_h2-h_lique-bossion_cr100/"]
+path_list += ["../../../output_data_2e5/shock_30_h2-h_lique-bossion_cr100/"]
+path_list += ["../../../output_data_2e5/shock_40_h2-h_lique-bossion_cr100/"]
+
+label_list = []
+label_list += ["20 km/s"]
+label_list += ["30 km/s"]
+label_list += ["40 km/s"]
+#plot_h2_coldens_compare(path_list, label_list)
+
+path_list = []
+path_list += ['../../../output_data_2e4/shock_30_h2-h_lique-bossion/']
+path_list += ['../../../output_data_2e4/shock_30_h2-h_wan/']
+
+label_list = []
+label_list += ["Flower"]
+label_list += ["Wan"]
+plot_h2_coldens_compare(path_list, label_list)
